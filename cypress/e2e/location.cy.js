9984ac7f0d710a2516fae6abde08b1bae1e80062
Am1d1v <dimadiman1adima@gmail.com>
2024-06-19 15:15:57 +0300

    display warning message that location fetching is blocked

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,8 +1,16 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
+
   it('should fetch the user location', () => {
     cy.visit('/');
     cy.get('[data-cy="get-loc-btn"]').click();
   });
+
+  it.only('should display warning message that location fetching is blocked', () => {
+    cy.visit('/');
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="info-message"]').contains('Your browser or permission settings do not allow location fetching');
+  });
+
 });
