9c8ff193b9977dfaa1dde6aebaba9894525eb1e7
Am1d1v <dimadiman1adima@gmail.com>
2024-06-19 16:25:34 +0300

    Creating Stub for getCurrentPosition

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -2,12 +2,19 @@
 
 describe('share location', () => {
 
-  it('should fetch the user location', () => {
-    cy.visit('/');
+  it.only('should fetch the user location', () => {
+    cy.visit('/').then(win => {
+      // Creating stub for navigator
+      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as('getUserPosition');
+    });
+
     cy.get('[data-cy="get-loc-btn"]').click();
+
+    // Check that getUserPosition have been called
+    cy.get('@getUserPosition').should('have.been.called');
   });
 
-  it.only('should display warning message that location fetching is blocked', () => {
+  it('should display warning message that location fetching is blocked', () => {
     cy.visit('/');
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('[data-cy="info-message"]').contains('Your browser or permission settings do not allow location fetching');
