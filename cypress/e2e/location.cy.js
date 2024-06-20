e527e92d93c92af1cdfca719cb0d1c16d08f60dd
Am1d1v <dimadiman1adima@gmail.com>
2024-06-20 18:06:44 +0300

    clipboard stub

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -2,7 +2,7 @@
 
 describe('share location', () => {
 
-  it.only('should fetch the user location', () => {
+  beforeEach(() => {
     cy.visit('/').then(win => {
       // Creating stub for navigator
       cy.stub(win.navigator.geolocation, 'getCurrentPosition')
@@ -17,8 +17,13 @@ describe('share location', () => {
             }, 300);
             
           });
-    });
 
+      cy.stub(win.navigator.clipboard, 'writeText').as('saveToClipboard').resolves();  
+    });
+  })
+
+  it('should fetch the user location', () => {
+    
     // Click on "Get Location" button
     cy.get('[data-cy="get-loc-btn"]').click();
 
@@ -39,22 +44,23 @@ describe('share location', () => {
 
   });
 
-  it('should display warning message that "Your Name" input field is empty', () => {
-    cy.visit('/').then(win => {
-      // Creating stub for navigator
-      cy.stub(win.navigator.geolocation, 'getCurrentPosition')
-        .as('getUserPosition')
-          .callsFake((cb) => {
+  it.only('should share a location URL', () => {
 
-            setTimeout(() => {
-              cb({coords: {
-                latitude: 36,
-                longitude: 45
-              }});
-            }, 300);
-            
-          });
-    });
+    // Select "Your Name" input field then type some dummy data
+    cy.get('[data-cy="name-input"]').type('Dima');
+
+    // Click on "Get Location" button
+    cy.get('[data-cy="get-loc-btn"]').click();
+
+    // Select "Share Link" button and click it
+    cy.get('[data-cy="share-loc-btn"]').click();
+  
+    // Save navigator data to the clipboard
+    cy.get('@saveToClipboard').should('have.been.called');
+
+  });
+
+  it('should display warning message that "Your Name" input field is empty', () => {
 
     // Click on "Get Location" button
     cy.get('[data-cy="get-loc-btn"]').click();
@@ -76,7 +82,6 @@ describe('share location', () => {
   });
 
   it('should display warning message that location fetching is blocked', () => {
-    cy.visit('/');
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('[data-cy="info-message"]').contains('Your browser or permission settings do not allow location fetching');
   });
